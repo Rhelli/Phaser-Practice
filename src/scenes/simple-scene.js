bff5207bf40405dfb6559d85d07856e630a29eb3
Rory Hellier <rory.hellier@googlemail.com>
2020-11-12 21:19:38 +0000

    Added phaser tutorial assets, began tutorial and created game world with platforms, player and cursors. Introduced physics and currently in the process of getting the keyboard controls to work

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,12 +1,73 @@
+let platforms;
+let player;
+let cursors;
+
 class SimpleScene extends Phaser.Scene {
   preload() {
-    this.load.image('cokecan', './cokecan.png');
+    this.load.image('sky', 'sky.png');
+    this.load.image('ground', 'platform.png');
+    this.load.image('star', 'star.png');
+    this.load.image('bomb', 'bomb.png');
+    this.load.spritesheet('dude', 'dude.png', { frameWidth: 32, frameHeight: 48 });
   }
 
   create() {
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.add.image(100, 200, 'cokecan');
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frame: [{ key: 'dude', frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
   }
+
+  //update() {
+  //  cursors = this.input.keyboard.createCursorKeys();
+//
+  //  if (cursors.left.isDown) {
+  //    player.setVelocityX(-160);
+  //    player.anims.play('left', true);
+  //  } else if (cursors.right.isDown) {
+  //    player.setVelocityX(160);
+  //    player.anims.play('right', true);
+  //  } else {
+  //    player.setVelocityX(0);
+  //    player.anims.play('turn');
+  //  }
+//
+  //  if (cursors.up.isDown && player.body.touching.down) {
+  //    player.setVelocityY(-330);
+  //  }
+  //}
+
+
 }
 
 export { SimpleScene };
